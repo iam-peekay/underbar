52484ef6a338809529d65a003c4640710938f917
Preethi Kasireddy <iam.preethi.k@gmail.com>
2015-08-07 19:57:58 -0700

    delay

--- src/underbar.js
+++ src/underbar.js
@@ -330,6 +330,10 @@ _.each = function(collection, iterator) {
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
   _.delay = function(func, wait) {
+    var args = Array.prototype.slice.call(arguments, 2);
+    setTimeout( function() {
+      func.apply(this, args);
+    }, wait);
   };
 
 
