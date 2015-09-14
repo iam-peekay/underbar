65fb4d50f11c6a5ff2c873fcf363b0654d64ad63
Preethi Kasireddy <iam.preethi.k@gmail.com>
2015-09-14 08:53:04 -0700

    add a comment in delay

--- src/underbar.js
+++ src/underbar.js
@@ -327,7 +327,7 @@ _.each = function(collection, iterator) {
   _.delay = function(func, wait) {
     var args = Array.prototype.slice.call(arguments, 2);
     setTimeout( function() {
-      func.apply(this, args);
+      func.apply(null, args); // NOTE TO SELF: Works with "null" or "this"
     }, wait);
   };
 
